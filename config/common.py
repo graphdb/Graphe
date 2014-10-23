be2ecb7df74da6a98ead1672fc37451bf4b294ce
L42y <423300@gmail.com>
2014-10-23 19:06:59 +0800

    fix(config): get correct database URI

--- config/common.py
+++ config/common.py
@@ -2,7 +2,11 @@
 
 import os
 
+from py2neo import ServiceRoot
+from py2neo.packages.httpstream.packages.urimagic import URI
+
 FACEBOOK_APP_ID = '539322962865082'
 FACEBOOK_APP_SECRET = 'bb32c614be336ba032605ef00873f1ed'
 NEO4J_DATABASE_URL = '%sdb/data/' % \
-                     os.environ.get('GRAPHENEDB_URL', 'http://localhost:7474/')
+                     URI(os.getenv('GRAPHENEDB_URL', ServiceRoot.DEFAULT_URI))\
+                         .resolve('/')
